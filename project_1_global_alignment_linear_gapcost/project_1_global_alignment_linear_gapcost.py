4ac0a31739b67fcaa947572931cb20f25797e886
Carl M. Kobel <kobel@pm.me>
2018-09-03 11:55:00 +0200

    backtrack works, but the track is too long

--- project_1_global_alignment_linear_gapcost/project_1_global_alignment_linear_gapcost.py
+++ project_1_global_alignment_linear_gapcost/project_1_global_alignment_linear_gapcost.py
@@ -1,4 +1,5 @@
-from pandas import DataFrame #pretty print result 2d-array
+import pandas #pretty print result 2d-array # too slow
+#import json
 #import numpy as np # overkill
 
 # Author: Carl M. Kobel 2018
@@ -11,26 +12,28 @@ from pandas import DataFrame #pretty print result 2d-array
 
 
 class Pairwise_alignment:
+    '''with linear gap cost'''
+
     def __init__(self, A, B):
-        # Constants
         
-        # self.A = A
-        # self.B = B
+        # Constants
         self.A = self.encode(A.upper())
         self.B = self.encode(B.upper())
 
         self.gap_cost = -5
         self.result = [[None for i in range(len(self.B) + 1)]\
-                        for i in range(len(self.A) + 1)]
+                       for i in range(len(self.A) + 1)]
 
         # vector = [[None for i in range(len(B) + 1)]\
         #                 for i in range(len(A) + 1)]
 
         self.score_matrix = [[10,  2,  5,  2], # A
-                        [ 2, 10,  2,  5], # C
-                        [ 5,  2, 10,  2], # G
-                        [ 2,  5,  2, 10]] # T
-                       #  A   C   G   T
+                             [ 2, 10,  2,  5], # C
+                             [ 5,  2, 10,  2], # G
+                             [ 2,  5,  2, 10]] # T
+                           #   A   C   G   T
+
+    # Helper methods
 
     def encode(self, input):
         return [int(i.replace('A', '0')\
@@ -45,12 +48,18 @@ class Pairwise_alignment:
                              .replace('3', 'T') for i in input)
 
 
-    
-
-
-    # Helper methods
     def drop_None(self, input):
         return [i for i in input if i != None]
+        # use enumerate instead?
+
+
+    def idx_of_max(self, input):
+        ''' Get indices of the maximum values in the input list '''
+        return [index for (index, value) in enumerate(input) if value == max(input)]
+
+
+    # core methods
+    
 
     def dyn_score(self, i, j):
         #print(f'{i},{j}  ', end = '') # debug
@@ -85,10 +94,46 @@ A ({self.decode(self.A)}) vertically
 B ({self.decode(self.B)}) horizontally
 
 score ({self.dyn_score(len(self.A), len(self.B))})
-{DataFrame(self.result)}
+{pandas.DataFrame(self.result)}
+''')
 
-        ''')
+    def backtrack(self):
 
 
-o = Pairwise_alignment('CGTGTCAAGTCT', 'ACGTCGTAGCTAGG')
+        def rec_backtrack(track, i, j):
+            ''' Recursive backtrack.
+            * Add option to save aligned string instead of track '''
+
+            track[i+j] = (i, j) # enter newly found position into track
+            print('i, j:', i, j)
+
+
+            candidates = []
+
+            if i > 0 and j > 0: 
+                candidates.append(self.result[i-1][j-1]) # diagonal
+            if i > 0 and j >= 0:
+                candidates.append(self.result[i-1][j]) # up
+            if i >= 0 and j > 0:
+                candidates.append(self.result[i][j-1]) # left
+            else: #
+                print(track) # base case
+
+            for candidate in self.idx_of_max(candidates): #"lc later"
+                print('candidates (list, idx):', candidates, candidate)
+                possibilities = [(i-1, j-1), (i-1, j), (i, j-1)]
+                print('possibilities', possibilities[candidate])
+                rec_backtrack(track, possibilities[candidate][0], possibilities[candidate][1])
+                    
+             
+        empty_track = [None for i in range(len(self.A) + len(self.B) + 1)] # An empty list with the length of the track
+        i, j = len(self.A), len(self.B) # Starting pointers, start in the bottom right corner
+        
+        rec_backtrack(empty_track, i, j)
+
+
+o = Pairwise_alignment('ACG', 'CGTG')
+
 o.complete()
+
+o.backtrack()
