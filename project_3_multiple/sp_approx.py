99087ba85c88a43bc9a368593e9770188f6ff643
cmk trashmaster <kobel@pm.me>
2018-09-26 18:16:55 +0200

    new files

---
+++ project_3_multiple/sp_approx.py
@@ -0,0 +1,3 @@
+# Author: Carl M. Kobel 2018
+
+# Implements the 2-approximation algorithm for any number of sequences (described in Section 8.2.2 in BA, or in Section 14.6.2 in Gusfield's book).
---
+++ project_3_multiple/sp_exact_3.py
@@ -0,0 +1,3 @@
+# Author: Carl M. Kobel 2018
+
+# Implements the exact algorithm for computing an optimal MSA of 3 sequences and its score (described on page 408 in BA, or in section 16.6.1 in Gusfield's book).
