0ad364405852ee5d3ba8bcb2b506883ec6f8bcd1
Carl M. Kobel <kobel@pm.me>
2018-10-06 21:28:12 +0200

    thnth

--- project_3_multiple/plot.R
+++ project_3_multiple/plot.R
@@ -5,28 +5,41 @@ exact = read_csv("Biologi/AiB/gr_AiB/project_3_multiple/running_time/sp_exact_3.
 
 data = bind_rows(approx, exact)
 
-
-
-
+# overview
 ggplot(data) +
     geom_line(aes(len, t,  color = alg)) +
     geom_point(aes(len,  t, color = alg)) +
-    labs(x = "sequence length (letters)", y = "time (seconds)", title = "Running time") + 
+    labs(x = "sequence length (letters)", y = "time (seconds)", title = "Running time of two algorithms") + 
     scale_color_discrete(guide = guide_legend(title = "algorithm"))
 
+# 2-approx normalized
+ggplot(filter(data, alg == "2-approx"), aes(len, t^(1/2)/len)) +
+    geom_line() +
+    geom_point() +
+    labs(x = "sequence length (letters)", y = "normalized time (t^(1/2) / |input|)", title = "Running time of 2-approx.")
 
-ggplot(filter(data, alg == "2-approx")) +
-    geom_line(aes(len, t^(1/2)/len))
+# exact 3 normalized
+ggplot(filter(data, alg == "exact 3"), aes(len, t^(1/3)/len)) +
+    geom_line() +
+    geom_point() +
+    labs(x = "sequence length (letters)", y = "normalized time (t^(1/3) / |input|)", title = "Running time of exact 3")
 
-ggplot(filter(data, alg == "exact 3")) +
-    geom_line(aes(len, t^(1/3)/len))
-
+# ratio
+max_ratio = tibble(
+    len = seq(10, 200),
+    r = 2-2/len,
+    algorithm = "theoretical max (2-2/m)"
+)
 
 ratios = approx$score/exact$score
-ratio_data = tibble(len = 1:length(ratios), r = ratios)
-ggplot(ratio_data) + 
-    geom_line(aes(len, r)) +
-    geom_point(aes(len, r)) +
-    labs(x = "sequence length (letters)", y = "ratio (approx/exact)", title = "Ratio between the score of two algorithms")
+ratio_data = tibble(len = 1:length(ratios)*10, r = ratios, algorithm = "measured") %>%
+    bind_rows(max_ratio)
+
+
+ggplot(ratio_data, aes(len, r, color = algorithm)) + 
+    geom_line() +
+    geom_point(data = filter(ratio_data, algorithm == "measured")) +
+    labs(x = "sequence length: m (letters)", y = "ratio (2_approx. / exact_3)", title = "Ratio between the score of two algorithms") +
+    theme(legend.position="bottom")
 
 
