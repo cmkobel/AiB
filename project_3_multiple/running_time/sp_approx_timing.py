5ddeaaac4d171af5478c9ef96133239e99b16325
Carl M. Kobel <kobel@pm.me>
2018-10-07 17:04:39 +0200

    new structure

---
+++ project_3_multiple/running_time/sp_approx_timing.py
@@ -0,0 +1,30 @@
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+
+
+import sp_approx
+import time
+
+for i in range(10, 210, 10):
+    
+    replicates = 10
+    mean_time_list = [None for i in range(replicates)]
+    mean_score = 0
+    for j in range(replicates):
+        t0 = time.time()
+
+        
+        o = sp_approx.SP_approx(f'../data/testseqs/testseqs_{i}_3.fasta')
+        score = o.build_alignment()[1]
+        
+        
+        t1 = time.time()
+        mean_time_list[j] = t1-t0
+        mean_score = score
+
+
+
+    print(f'{i}, {sum(mean_time_list)/replicates}, {mean_score}')
+    
---
+++ project_3_multiple/running_time/sp_exact_3_timing.py
@@ -0,0 +1,31 @@
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+
+import sp_exact_3
+import time
+
+for i in range(10, 210, 10):
+    
+    replicates = 10
+    mean_time_list = [None for i in range(replicates)]
+    mean_score = 0
+    for j in range(replicates):
+        t0 = time.time()
+        
+
+        o = sp_exact_3.SP_exact_3(f'../data/testseqs/testseqs_{i}_3.fasta')
+        #score = o.align(trace = False)[len(o.A)][len(o.B)][len(o.C)]
+        score = o.align(trace = False)#[-1][-1][-1]#[len(o.A)][len(o.B)][len(o.C)]
+
+
+
+        t1 = time.time()
+        mean_time_list[j] = t1-t0
+        mean_score = score
+
+
+
+    print(f'{i}, {sum(mean_time_list)/replicates}, {mean_score}')
+    
