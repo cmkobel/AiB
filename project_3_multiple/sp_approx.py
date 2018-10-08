907326a36ad9f4ca0e216e26f996b931ad71caef
Carl M. Kobel <kobel@pm.me>
2018-10-08 10:40:46 +0200

    nam

--- project_3_multiple/sp_approx.py
+++ project_3_multiple/sp_approx.py
@@ -1,5 +1,5 @@
 from Bio import SeqIO
-# Author: Carl M. Kobel 2018
+# Author: Carl Mathias Kobel 2018
 
 # Implements the 2-approximation algorithm for any number of sequences
 #(described in Section 8.2.2 in BA, or in Section 14.6.2 in Gusfield's book).
--- project_3_multiple/sp_exact_3.py
+++ project_3_multiple/sp_exact_3.py
@@ -1,5 +1,5 @@
 from Bio import SeqIO
-# Author: Carl M. Kobel 2018
+# Author: Carl Mathias Kobel 2018
 
 # Implements the exact algorithm for computing an optimal MSA of 3 sequences and its score (described on page 408 in BA, or in section 16.6.1 in Gusfield's book).
 
