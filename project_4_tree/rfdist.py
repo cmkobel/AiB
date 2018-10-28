b97eeb4305225d61f74f3fe79e8398c87e572e90
Carl M. Kobel <kobel@pm.me>
2018-10-28 19:19:47 +0100

    class-ized

--- project_4_tree/rfdist.py
+++ project_4_tree/rfdist.py
@@ -2,74 +2,69 @@ from Bio import Phylo as ph
 
 # author: Carl Mathias Kobel
 
+class Robinson_Foulds_distance:
+    def __init__(self, file1, file2):
+        self.tree1 = ph.read(file1, "newick")
+        self.tree2 = ph.read(file2, "newick")
 
-tree0 = ph.read("data/Testdata/tree0.new", "newick")
-tree1 = ph.read("data/Testdata/tree1.new", "newick")
-tree2 = ph.read("data/Testdata/tree2.new", "newick")
+        self.distance = self.rfdist()
 
-
-ph.draw_ascii(tree0)
-
-
-
-
-def is_leaf(node):
-    return len(node.clades) == 0
-
-
-def terminals(root):
-    """ Takes a root node, and returns the terminal clades (leaves). """
-    leaves = []
     
-    def rec(clade):
-        """ Recursively traverses the tree, looking for terminals. """
-        #if clade.is_terminal():
-        if is_leaf(clade):    
-            #leaves.append(clade)
-            leaves.append(clade.name) # easier, but less flexible
-            return
-        else:
+    def is_leaf(self, node):
+        return len(node.clades) == 0
+
+
+    def terminals(self, root):
+        """ Takes a root node, and returns the terminal clades (leaves). """
+        leaves = []
+        
+        def rec(clade):
+            """ Recursively traverses the tree, looking for terminals. """
+            #if clade.is_terminal():
+            if self.is_leaf(clade):    
+                #leaves.append(clade)
+                leaves.append(clade.name) # easier, but less flexible
+                return
+            else:
+                for clade in clade.clades:
+                    rec(clade)
+
+        rec(root)
+        leaves.sort() # sort to get comparable lists
+        return leaves
+
+
+    def internals(self, root):
+        """ Traverses the internal elements. 
+        Could have been a generator. """
+        rv = []
+        
+        def rec(clade):
+            """ Recursively traverses the tree, looking for internals. """
+            if not self.is_leaf(clade):
+                rv.append(clade)
+
             for clade in clade.clades:
                 rec(clade)
 
-    rec(root)
-    leaves.sort() # sort to get comparable lists
-    return leaves
-
-#test:
-#print(terminals(tree0.root))
+        rec(root)
+        return rv
 
 
-def internals(root):
-    """ Traverses the internal elements. 
-    Could have been a generator. """
-    rv = []
-    
-    def rec(clade):
-        """ Recursively traverses the tree, looking for internals. """
-        if not is_leaf(clade):
-            rv.append(clade)
-
-        for clade in clade.clades:
-            rec(clade)
-
-    rec(root)
-    return rv
-
-#test:
-#print(internals(tree0.root))
+    def rfdist(self):
+        shared = 0
+        for i_1 in self.internals(self.tree1.root):
+            for i_2 in self.internals(self.tree2.root):
+                if self.terminals(i_1) == self.terminals(i_2):
+                    shared += 1
+        return len(self.internals(self.tree1.root)) + len(self.internals(self.tree2.root)) - 2*shared
 
 
-shared = 0
-non_shared = 0
-for i_1 in internals(tree1.root):
-    for i_2 in internals(tree2.root):
-        if terminals(i_1) == terminals(i_2):
-            shared += 1
 
-#print(shared)
 
-# assuming num of internal is the same as splits, probably not true for polytomous trees
-print(len(internals(tree1.root)) + len(internals(tree2.root)) - 2*shared)
+if __name__ == "__main__":
+    o = Robinson_Foulds_distance("data/Testdata/tree1.new",
+                                 "data/Testdata/tree2.new")
+    print(o.distance)
 
-print(tree0.root.clades[1])
+
