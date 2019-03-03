7866c0cb2428da857f6863b265951447a8758041
Carl M. Kobel <kobel@pm.me>
2019-03-03 11:18:38 +0100

    alt virker

--- project_7_suffix_trees/parsers.py
+++ project_7_suffix_trees/parsers.py
@@ -1,4 +1,5 @@
 # Author: Carl M. Kobel 2019
+import json
 
 
 def parse_fasta(input_file):
@@ -9,18 +10,28 @@ def parse_fasta(input_file):
     header_lines = [_i for _i, i in enumerate(raw) if i[0:1] == ">"] # alle linjenumre hvor der er en header.
     end_lines = [i-1 for i in header_lines[1:]] + [len(raw)] # linjetal minus en, fra anden header til slut, plus sidste linje.
 
+
+
     for start, end in zip(header_lines, end_lines):
+        #print(start, end)
         rv = {}
         rv['title'] = raw[start][1:].strip()
-        rv['sequence'] = ''.join(raw[start+1:end])
+        rv['sequence'] = ''.join(raw[start+1:end+1])
         yield rv
 
 
 if __name__ == "__main__":
-    for reference in parse_fasta('data/ref.fa'):
-        print(reference['title'])
-        print('\t', reference['sequence'])
-        print()
+
+
+    for genome in parse_fasta('data/seqs_mut.fasta'):
+        #print('genome:', genome['sequence'])
+        print(json.dumps(genome))
+
+    if not False:
+        for reference in parse_fasta('data/ref.fa'):
+            print(reference['title'])
+            print('\t', reference['sequence'])
+            print()
 
 
 def parse_fastq(input_file):
--- project_7_suffix_trees/sam_out.py
+++
@@ -1,8 +0,0 @@
-from search_st import suffixtree
-
-S = 'Mississippi'
-st = suffixtree(S, show = False)
-print('tree done')
-match = st.find_positions('iss')
-
-print(match)
